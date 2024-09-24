bea1d01c7a8b21f603700f691f59eee4625c3568
Daniel Amaral <silva.daniel86@gmail.com>
2024-09-24 01:36:07 -0300

    ci: update gh actions

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -1,9 +1,8 @@
-import os
+from glob import glob
 import pandas as pd
 
 if __name__ == '__main__':
-    print(os.listdir('./data'))
-    da_fiis = pd.read_csv('data/ranked_fiis_*.csv') 
+    da_fiis = pd.read_csv(glob("data/ranked_fiis_*.csv")[0])
     da_fiis_top_15 = da_fiis.head(15)
 
     markdown_table = da_fiis_top_15.to_markdown(index=False, numalign="left", stralign="left")
