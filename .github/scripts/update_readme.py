4285f20ccd4395123e7d88673afeb1db6bb4c717
Daniel Amaral <silva.daniel86@gmail.com>
2024-09-24 11:52:48 -0300

    ci: update gh actions

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -2,10 +2,16 @@ from glob import glob
 import pandas as pd
 
 if __name__ == '__main__':
-    da_fiis = pd.read_csv(glob("data/ranked_fiis_*.csv")[0])
-    da_fiis_top_15 = da_fiis.head(15)
+    da_fiis_raw = pd.read_csv(glob("data/ranked_fiis_*.csv")[0])
 
-    markdown_table = da_fiis_top_15.to_markdown(index=False, numalign="left", stralign="left")
+    da_fiis = pd.DataFrame({
+        'Código': f'[{da_fiis_raw['ticker']}](https://www.fundsexplorer.com.br/funds/{da_fiis_raw['ticker'].str.lower()})',
+        'Score': f"{(da_fiis_raw['score'] * 100):.1f}%",
+        'Setor(es)': da_fiis_raw['setor'],
+        'DY (12M)': f"{da_fiis_raw['dy_12m_acumulado']:.2f}"
+    }).head(15) # top 15
+
+    markdown_table = da_fiis.to_markdown(index=False, numalign="left", stralign="left")
 
     with open('.github/templates/README.md', 'r') as f:
         readme_template = f.read()
